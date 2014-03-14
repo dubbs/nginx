72264f1642d5f51f05944fc1fed2535def2192f0
Kevin Warrington <kevin.warrington@gmail.com>
2014-03-13 22:10:28 -0600

    initial

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y nginx
+
+rm -rf /var/www
+rm /etc/nginx/sites-enabled/default
+
+ln -fs /vagrant/public /var/www
+ln -fs /vagrant/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+
+sudo service nginx restart
